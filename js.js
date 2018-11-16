3b1fc92c920f9dc4acf5a0e8617750e6e0b1c3a7
daneelsheleg <42346177+daneelsheleg@users.noreply.github.com>
2018-11-16 23:22:10 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -6,13 +6,11 @@ document.addEventListener('DOMContentLoaded', function (params) {
     var emailElem = document.querySelector('#email');
     var nameElem = document.querySelector('#name');
     var oneElem = document.querySelector('#one');
-   
        
-    function isFieddFill(nameElem) {
-        return (nameElem.getAttribute('required'))
-            nameElem.value.length > 0;
-    };
-   
+
+function isFieldFill(input) {
+    return ((input.hasAttribute('required')) && (input.value.length>0));
+}
 
     submitElem.addEventListener('click', function (event) {
         event.preventDefault();
@@ -25,29 +23,49 @@ document.addEventListener('DOMContentLoaded', function (params) {
             var pattern = nameElem.getAttribute('pattern');
              var reg = new RegExp(pattern);
              if (!reg.test(nameElem.value)){
-                massEror.push('Заполните правильно!!!');
+                massEror.push('Заполните правильно - ИМЯ !!!');
              } 
             };
             if (emailElem.hasAttribute('pattern')) {
                 var pattern = emailElem.getAttribute('pattern');
                  var reg = new RegExp(pattern);
                  if (!reg.test(emailElem.value)){
-                    massEror.push('Заполните правильно!!!');
+                    massEror.push('Заполните правильно - EMAIL !!!');
                  } 
                 };
                 if ( passwordElem.hasAttribute('pattern')) {
                     var pattern =  passwordElem.getAttribute('pattern');
                      var reg = new RegExp(pattern);
                      if (!reg.test(passwordElem.value)){
-                        massEror.push('Заполните правильно!!!');
+                        massEror.push('Заполните правильно - ПАРОЛЬ!!!');
                      } 
                     }
-                    
-
         console.log(massEror);
     });
+    
+   
+     var div = document.createElement('div');
+    
+      submitElem.addEventListener('click', function () { 
+         div.innerText = '';
+             for(  var i = 0;  i <  massEror.length; i++){
+              
+                 var p = document.createElement('p');
+              
+                div.appendChild(p);
+                p.innerText = massEror[i];
+                div.style = 'justify-content:center'
+                 p.style = 'text-align:center; color:white';
+                
+            }
+    })
+      document.body.appendChild(div);
 
+       submitElem.addEventListener('click', function () { 
 
-
-
+          if ( emailElem.value == 0){
+             var color = document.querySelector('#color');
+                color.style = 'color:red';
+        };
+       });
 });
