f1aacaf59e59f5d8d0227f276e3361b78dc5994c
daneelsheleg <42346177+daneelsheleg@users.noreply.github.com>
2018-11-18 22:20:05 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -6,66 +6,55 @@ document.addEventListener('DOMContentLoaded', function (params) {
     var emailElem = document.querySelector('#email');
     var nameElem = document.querySelector('#name');
     var oneElem = document.querySelector('#one');
-       
+    var div = document.createElement('div');
 
 function isFieldFill(input) {
-    return ((input.hasAttribute('required')) && (input.value.length>0));
-}
-
+    return ((input.hasAttribute('required')) && (input.value.length == 0));
+};
+  
     submitElem.addEventListener('click', function (event) {
         event.preventDefault();
         massEror = [];
-
-        if (passwordElem.value !== configElem.value){
-            massEror.push('пароли не совпадают');
-        };
-        if (nameElem.hasAttribute('pattern')) {
-            var pattern = nameElem.getAttribute('pattern');
-             var reg = new RegExp(pattern);
-             if (!reg.test(nameElem.value)){
-                massEror.push('Заполните правильно - ИМЯ !!!');
-             } 
+        div.innerText = '';
+            if (passwordElem.value !== configElem.value){
+                massEror.push('пароли не совпадают');
             };
-            if (emailElem.hasAttribute('pattern')) {
-                var pattern = emailElem.getAttribute('pattern');
-                 var reg = new RegExp(pattern);
-                 if (!reg.test(emailElem.value)){
-                    massEror.push('Заполните правильно - EMAIL !!!');
-                 } 
+                if (nameElem.hasAttribute('pattern')) {
+                        var pattern = nameElem.getAttribute('pattern');
+                        var reg = new RegExp(pattern);
+                    if (!reg.test(nameElem.value)){
+                         massEror.push('Заполните правильно - ИМЯ !!!');
+                    } 
                 };
-                if ( passwordElem.hasAttribute('pattern')) {
-                    var pattern =  passwordElem.getAttribute('pattern');
-                     var reg = new RegExp(pattern);
-                     if (!reg.test(passwordElem.value)){
-                        massEror.push('Заполните правильно - ПАРОЛЬ!!!');
-                     } 
-                    }
-        console.log(massEror);
-    });
-    
-   
-     var div = document.createElement('div');
-    
-      submitElem.addEventListener('click', function () { 
-         div.innerText = '';
+                    if (emailElem.hasAttribute('pattern')) {
+                            var pattern = emailElem.getAttribute('pattern');
+                            var reg = new RegExp(pattern);
+                        if (!reg.test(emailElem.value)){
+                                massEror.push('Заполните правильно - EMAIL !!!');
+                            if (emailElem.value.length == 0){
+                                var color = document.querySelector('#color');
+                                color.style = 'color:red';
+                            };
+                            };
+                    };
+                        if ( passwordElem.hasAttribute('pattern')) {
+                                var pattern =  passwordElem.getAttribute('pattern');
+                                var reg = new RegExp(pattern);
+                            if (!reg.test(passwordElem.value)){
+                                massEror.push('Заполните правильно - ПАРОЛЬ!!!');
+                                } 
+                            };
+                          
              for(  var i = 0;  i <  massEror.length; i++){
-              
-                 var p = document.createElement('p');
-              
+                var p = document.createElement('p');
                 div.appendChild(p);
                 p.innerText = massEror[i];
-                div.style = 'justify-content:center'
-                 p.style = 'text-align:center; color:white';
-                
+                    div.style = 'justify-content:center'
+                    p.style = 'text-align:center; color:white';
             }
-    })
-      document.body.appendChild(div);
 
-       submitElem.addEventListener('click', function () { 
+        console.log(massEror);
+        document.body.appendChild(div);
 
-          if ( emailElem.value == 0){
-             var color = document.querySelector('#color');
-                color.style = 'color:red';
-        };
-       });
+    });
 });
